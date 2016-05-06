8bac3f2384f259120164cbd83750e16fd60d2253
Anupam Debnath <adebnath1989@gmail.com>
2016-05-06 18:22:47 +0530

    fetch second collection

--- index.js
+++ index.js
@@ -7,22 +7,76 @@ var MongoClient = mongodb.MongoClient;
 
 
 // Connection URL. This is where your mongodb server is running.
-var url = 'mongodb://localhost:27017/anupam';
+var url = 'mongodb://localhost:27017/testlist';
+
+var ObjectId = require('mongodb').ObjectID;
+
+//Initializing empty arrays for cycle and bugs
+var cycle = [];
+var bugs = [];
 
 
-// Use connect method to connect to the Ser
+// Use connect method to connect to the Server
 MongoClient.connect(url, function (err, db) {
 
     // Get the collection
-    var collection = db.collection('testcoll');
+    var collection = db.collection('buglist');
+    var linecollection = db.collection('linecoll');
+    
 
-    // Using Cursor
-    collection.find().forEach( function(myDoc) { 
-    	console.log( "name: " + myDoc.tab ); 
+    // Getting buglist data
+	    //collection.find().forEach(function(myDoc) { 
+	    collection.find().toArray(function(myDoc) {
 
-	}); 
+	    	console.log( "name: " + myDoc.cycle );
 
-      
+	    	// Pushing the data to an array and later we would push it to another collection
+	    	cycle.push(myDoc.cycle); 
+	    	bugs.push(myDoc.bugs);
+
+	    	// Printing the arrays
+	    	console.log(cycle);
+	    	console.log(bugs);
+
+		    	// Getting linecollec data:
+		    	//linecollection.find().forEach(function(doc){
+
+		    	//	console.log("Line collection data =========>")
+		    	//	console.log(doc);
+		    	//	console.log("Buglist ++++++++++",bugs)
+
+		    	linecollection.update({"_id" : ObjectId("5716330d7745607233856db2")},{$push: {"data":10000}}, function(err,docs){
+
+		    		if (err){
+		    			console.log(err);
+		    		} else {
+		    			console.log("pushing the data");
+		    		}
+		    	});
+
+		    		
+		    		
+
+		}); 	       
 });
 
-//console.log(result)
+
+/*
+MongoClient.connect(url, function(err,db){
+	if (err) {
+		console.log(err)
+	} else {
+
+		//Get the collection
+		var linecoll = db.collection('linecoll');
+
+
+		linecoll.find().forEach(function(doc){
+			console.log("Line Collection data ------>")
+			console.log(doc);
+
+		db.close();
+
+		});
+	};
+}); */
